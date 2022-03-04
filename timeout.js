91bc51cb0d4fab0f4dbc81d9457f6bf4c1d69957
Nurul Islam <nurulbdpersonal@gmail.com>
2022-03-04 20:03:43 +0600

    first commit

---
+++ timeout.js
@@ -0,0 +1,25 @@
+/*
+function doSomething() {
+    console.log('I am coding JavaScript');
+}
+console.log('First: ami sobar age');
+console.log('Second: I am the second person');
+// setTimeout(doSomething, 6000);
+setTimeout(function () {
+    console.log('I am using VS Code');
+}, 5000);
+setTimeout(() => {
+    console.log('Exploring MDN articles');
+}, 4000);
+console.log('Third: ami 3 number baccha');
+console.log('Fourth: ami ek hali purno korsi'); */
+
+// **********Quiz area *************
+
+ /* 
+ // Quiz 38, Number 11
+setTimeout(() =>console.log("I am Superman"),1000)
+console.log("I am Superwoman");
+// rs I am Superwoman
+//    I am Superman   */ 
+
